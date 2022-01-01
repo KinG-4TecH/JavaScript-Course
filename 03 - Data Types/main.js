f8223fcb712f476effbbb85da49c1c2f8aa75986
KinG4TecH <91432207+KinG-4TecH@users.noreply.github.com>
2022-01-01 13:42:13 +0200

    Update main.js

--- 03 - Data Types/main.js
+++ 03 - Data Types/main.js
@@ -1,9 +1,18 @@
-let a = 3 // number
-let b = 3.01 // number
-let c = "Hello World" // String
-let d = `Hello World` // String
-let e = 'Hello World' // String
-let f = true // Boolean
-let g = false // Boolean
-let h = undefiend // undefiend
-let i = null //Object
+/*
+    - number
+    - string
+    - boolean
+    - object => Array
+    - object => JSON
+*/
+
+
+var a = `mohammad` // blog section
+console.log(typeof(a))
+var b = 1
+console.log(typeof(b))
+var c = true
+console.log(typeof(c))
+var d = [1,2,3,4]
+console.log(typeof(d))
+var e = {name: "mohammad", age: 16, height: 172};
