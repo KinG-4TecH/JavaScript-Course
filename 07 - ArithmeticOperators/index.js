c5722e81552b3b4814016c84a95ebe488e515644
MohammadSawan <kinggamerads@gmail.com>
2022-02-05 01:46:40 +0200

    ArithmeticOperators

---
+++ 07 - ArithmeticOperators/index.js
@@ -0,0 +1,24 @@
+/* 
+    + ;
+    - ;
+    * ;
+    / ;
+    % ;
+    ** ;
+    ++ ;
+    == ;
+
+*/
+
+let a = 2;
+let b = 2;
+// a++;
+// console.log(a);
+console.log(a++);
+console.log(++b);
+
+let c = 2;
+let d = 2;
+
+console.log(c--);
+console.log(--d);
