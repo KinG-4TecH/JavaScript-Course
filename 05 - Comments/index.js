eaf6e20c7be20f324ee7fdc4aef90998295e905d
MohammadSawan <kinggamerads@gmail.com>
2022-02-05 01:27:15 +0200

    comments

---
+++ 05 - Comments/index.js
@@ -0,0 +1,17 @@
+// 1st
+/* 2nd */
+
+// say hello world in console
+
+/*console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+*/
+
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+console.log("Hello World");
+
+// in vs code you can comment with ctrl + /
