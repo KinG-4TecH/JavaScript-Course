95a9fb68ff135e3e3aa73d94a3479d2097c13dbb
MohammadSawan <kinggamerads@gmail.com>
2022-02-05 01:58:50 +0200

    AssignmentOperators

---
+++ 08 - AssignmentOperators/index.js
@@ -0,0 +1,25 @@
+let a = 10;
+// a = a + 10;
+a += 10;
+// console.log(a);
+
+let b = 10;
+// b = b - 5;
+b -= 5;
+
+// console.log(b + 3);
+
+// console.log(b ** 2);
+
+// console.log(a + b);
+
+// console.log(a + b - 20 + "    Hello Youtube");
+
+// console.log(a + b );
+//console.log(`${a + b - 20} Hello Youtube`);
+
+let c = 0;
+
+c = b + 5 - 2;
+
+console.log(c + ((100 / 2) % 3));
