785615e7388d12da9cb9962ed134227f60f17314
MohammadSawan <kinggamerads@gmail.com>
2022-02-05 13:19:05 +0200

    Comparison Operators

---
+++ 09 - ComparisonOperators/index.js
@@ -0,0 +1,28 @@
+/* 
+ == Equal
+ === Identical
+ > Greater Than
+ < Less Than
+ >= Greater Than OR Equal
+ <= Less Than OR Equal
+ != Not Equal
+ !== Not Equal value or Not Equal Type 
+*/
+
+let a = 10;
+
+// console.log(a == "10");
+
+// console.log(a === "10");
+
+// console.log(a > 10);
+
+// console.log(a < 10);
+
+// console.log(a >= 10);
+
+// console.log(a <= 10);
+
+// console.log(a != 20);
+
+// console.log(a !== "10");
