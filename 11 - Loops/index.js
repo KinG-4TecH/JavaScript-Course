c1a24afe608cbb722114f2deaf71a1c67bd7e7a1
MohammadSawan <kinggamerads@gmail.com>
2022-02-07 22:59:20 +0200

    Loops

---
+++ 11 - Loops/index.js
@@ -0,0 +1,33 @@
+var languages = ["JavaScript", "C#", "C++", "HTML", "CSS", "Python"];
+// console.log(languages.length);
+
+// for (let i = 0; i < languages.length; i++) {
+//   if (languages[i] === "JavaScript") {
+//     console.log(`Javascript language found in ${i + 1} in languages array`);
+//   }
+// }
+let x = "JavaScript";
+
+// for (let i in languages) {
+// console.log(i);
+//   if (languages[i] === "JavaScript") {
+//     console.log(
+//       `Javascript language found in ${parseInt(i) + 1} in languages array`
+//     );
+//   }
+// }
+
+// for (let i of languages) {
+//   console.log(parseInt(i));
+// }
+
+// let a = 10;
+// while (a < 20) {
+//   a++;
+// console.log(a);
+// }
+
+// do {
+//   a++;
+//   console.log(a);
+// } while (a < 20);
