54dffc50257295c2ef2869380fc83ba0d007c5cf
MohammadSawan <kinggamerads@gmail.com>
2022-02-10 00:44:11 +0200

    Arrays

---
+++ 12 - Arrays/index.js
@@ -0,0 +1,20 @@
+var languages = ["JavaScript", "C#", "C++", "HTML", "CSS", "Python"];
+var names = new Array("Mhmd", "Sami", "Nour", "Hiba");
+
+var number = 12;
+
+// languages.pop(); Remove last element
+// languages.shift(); Remove first element
+// languages.splice(3, 1); delete element by index and number of delete count
+// languages.push("PHP"); add element to array
+// languages.unshift("PHP"); add element to first of array
+// languages.length = 8;
+// languages.sort();
+// languages.reverse();
+
+// languages.splice(languages.indexOf("HTML"), 1);
+// if (languages.includes("HTML")) {
+//   console.log("Yes and index of it  " + languages.indexOf("HTML"));
+// }
+
+console.log(languages);
